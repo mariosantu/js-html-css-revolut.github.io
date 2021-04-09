1369f842ee34dd76168cf0ead0352a74540a01d2
Mario Santucci <mario.santucci95@gmail.com>
2021-04-09 16:32:29 +0200

    add chevron up and down

--- js/script.js
+++ js/script.js
@@ -7,7 +7,17 @@ $(document).ready(function() {
     $('.menu_dx li').mouseleave(function() {
         $(this).children('.dropdown').removeClass('active');
     });
-    
+
+    $('.chevroned a').mouseenter(function() {
+        $(this).children('i').removeClass('fa-chevron-down');
+        $(this).children('i').addClass('fa-chevron-up');
+    });
+
+    $('.chevroned a').mouseleave(function() {
+        $(this).children('i').removeClass('fa-chevron-up');
+        $(this).children('i').addClass('fa-chevron-down');
+    });
+
 });
 
 
